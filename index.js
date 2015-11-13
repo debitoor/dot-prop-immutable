16a5b044ebf352130af6343b0142f0d2df125887
Dan Rasmussen <dra@e-conomic.com>
2015-11-13 12:21:47 +0100

    refactor

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function setProp(obj, prop, value) {
 	var setPropImmutableRec = function(obj, prop, value, i) {
 		var head = prop[i];
 
-		if (prop.length - 1 !== i) {
+		if (prop.length !== i) {
 			if (Array.isArray(obj)) {
 				if (/^\+?\d+$/.test(head) && obj.length > head) {
 					return [...obj.slice(0, head), setPropImmutableRec(obj[head] || {}, prop, value, i + 1), ...obj.slice(head + 1)];
@@ -21,19 +21,7 @@ function setProp(obj, prop, value) {
 			}
 		}
 
-		if (Array.isArray(obj)) {
-			if (/^\+?\d+$/.test(head) && obj.length > head) {
-				return [...obj.slice(0, head), typeof value === 'function' ? value(obj[head]) : value, ...obj.slice(head + 1)];
-			} else {
-				let clone = [...obj];
-				clone[head] = typeof value === 'function' ? value(obj[head]) : value;
-				return clone;
-			}
-
-		} else {
-
-			return Object.assign({}, obj, {[head]: typeof value === 'function' ? value(obj[head]) : value});
-		}
+		return typeof value === 'function' ? value(obj) : value;
 	};
 
 	return setPropImmutableRec(obj, prop, value, 0);
