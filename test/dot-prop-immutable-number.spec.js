cdccb5681bf8636cde12a23879711b852207f5bb
Steven Spungin <steven@spungin.tv>
2016-12-28 15:24:16 -0500

    Add more tests

---
+++ test/dot-prop-immutable-number.spec.js
@@ -0,0 +1,62 @@
+var dotProp = require('..');
+
+describe('dot-prop-immutable.number.spec.js', function () {
+
+	var arr = [1, {a: false}];
+
+	var result;
+	describe('when have an array', () => {
+
+		describe('when set prop using number as path', () => {
+
+			before(function () {
+				result = dotProp.set(arr, 1, 3);
+			});
+
+			it('should replace prop', () => {
+				expect(result).to.eql([
+				1,
+				3
+				]);
+			});
+
+			it('invariant', arrInvariant);
+		});
+
+
+		describe('when get prop using number as path', () => {
+
+			before(function () {
+				result = dotProp.get(arr, 1);
+			});
+
+			it('should get prop', () => {
+				expect(result).to.eql({a: false});
+			});
+
+			it('invariant', arrInvariant);
+		});
+
+		describe('when delete prop using number as path', () => {
+
+			before(function () {
+				result = dotProp.delete(arr, 1);
+			});
+
+			it('should remove prop', () => {
+				expect(result).to.eql([
+				1
+				]);
+			});
+			it('invariant', arrInvariant);
+		});
+	});
+
+	function arrInvariant() {
+		expect(arr).to.eql(
+		[1, {a: false}]
+		);
+	}
+});
+
+
