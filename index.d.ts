02670a10cc4ce88076201c5df01bf4aac5109523
Daniel Hillmann <hiller@live.dk>
2019-08-22 11:15:36 +0200

    changes

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export function set(obj: any, prop: number | string, value: any): any;
 export function get(obj: any, prop: number | string, value?: any): any;
 export function remove(obj: any, prop: number | string): any;
-export function toggle(obj: any, prop: number | string): T;
-export function merge(obj: any, prop: number | string, val: any): T;
+export function toggle(obj: any, prop: number | string): any;
+export function merge(obj: any, prop: number | string, val: any): any;
