fb60dd590d0d897afb4d27dfa7e5a4ebf5690400
Daniel Hillmann <hiller@live.dk>
2019-10-08 14:02:43 +0200

    optimise index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,9 @@
+declare const dotProp: {
+	delete: (obj: any, prop: number | string) => any;
+	set: (obj: any, prop: number | string, value: any) => any;
+	get: (obj: any, prop: string | string[] | number, value?: any) => any;
+	toggle: (obj: any, prop: number | string) => any;
+	merge: (obj: any, prop: number | string, val: any) => any;
+}
 
-function set(obj: any, prop: number | string, value: any): any;
-function get(obj: any, prop: string | string[] | number, value?: any): any;
-function toggle(obj: any, prop: number | string): any;
-function merge(obj: any, prop: number | string, val: any): any;
-function _delete(obj: any, prop: number | string): any;
-
-declare const dotPropImmutable = {
-	delete: _delete,
-	set,
-	get,
-	toggle,
-	merge
-};
-
-export = dotPropImmutable;
+export = dotProp;
