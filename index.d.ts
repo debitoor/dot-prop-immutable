b9801aa52e1be09d9ad6ee7d7553ff73038cd923
Daniel Hillmann <hiller@live.dk>
2020-07-15 09:01:57 +0200

    typings changes

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,12 @@
+type Path = number | string | (string | number)[];
+
 interface DotPropImmutable {
-  get<I, T = any>(source: I, path: number | string | (string | number)[], defaultValue?: any): T;
-  set<I, T = any>(source: I, path: number | string | (string | number)[], value: any): T;
-  delete<I, T = any>(source: I, path: number | string | (string | number)[]): Partial<T>;
-  toggle<I, T = any>(source: I, path: number | string | (string | number)[]): T;
-  merge<I, T = any>(source: I, path: number | string | (string | number)[], value: any): T;
+  get<I, T = any>(source: I, path: Path, defaultValue?: any): T;
+  set<I, T = any>(source: I, path: Path, value: any): T;
+  setMany<I, T = any>(source, paths: any)
+  delete<I, T = any>(source: I, path: Path): Partial<T>;
+  toggle<I, T = any>(source: I, path: Path): T;
+  merge<I, T = any>(source: I, path: Path, value: any): T;
 }
 
 declare const dotPropImmutable: DotPropImmutable;
